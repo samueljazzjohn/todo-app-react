059a51d3ae03801359b833f4ea3c430981009714
samuel jazz john <samueljazzjohn@gmail.com>
2020-12-16 12:42:02 +0530

    background styles changed for list items and alert

--- src/todo.js
+++ src/todo.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react';
-import {UncontrolledAlert,Alert,Col,Row,Button,Input,InputGroup,InputGroupAddon,InputGroupText, Container, Table} from 'reactstrap';
+import {ListGroup,ListGroupItem,UncontrolledAlert,Alert,Col,Row,Button,Input,InputGroup,InputGroupAddon,InputGroupText, Container, Table} from 'reactstrap';
 import './todo.css';
 import { transitions, positions, Provider as AlertProvider } from 'react-alert'
 import AlertTemplate from 'react-alert-template-basic'
@@ -72,54 +72,37 @@ class Todo extends Component{
                 <Container>
                     <Row>
                         <Col className="pt-2">
-                            <Alert color="danger" isOpen={this.state.visible} toggle={this.toggle}>
+                            <Alert color="danger" isOpen={this.state.visible} toggle={this.toggle} id="alert">
                                 {this.state.alert}
                             </Alert>
                         </Col>
                     </Row>
                     <Row className="p-4  mt-5 justify-content-center" id="header">
-                        <Col md={6} className="pt-5  addfield ">
-                            <InputGroup >
-                                <Input id="search" lassName="ml-2 mt-2  text-center" value={this.state.textEntered} placeholder="Add qualification details" type="text" onChange={this.showText}/>
+                        <Col lg={6} className="pt-5  addfield ">
+                            <InputGroup id="searchparent">
+                                <Input id="search" className="  text-center" value={this.state.textEntered} placeholder="Add qualification details" type="text" onChange={this.showText}/>
                             </InputGroup>
                         </Col>
-                        <Col md={4} className="pt-5 pb-5 addfield text-center">
-                            <Button id='button'  className="pl-4 pr-4  " color="danger" onClick={this.addToScreen} >ADD </Button>{' '}
-                            <Button id='button' className="  " color="danger" onClick={this.clear}>CLEAR</Button>{' '}
+                        <Col lg={4} className="pt-5 pb-5 addfield text-center">
+                            <Button id='button'  className="pl-4 pr-4  "  onClick={this.addToScreen} >ADD </Button>{' '}
+                            <Button id='button' className="  " onClick={this.clear}>CLEAR</Button>{' '}
                         </Col>
                     </Row>
 
-
-                    {/* <Row className="p-3 pl-5 mt-3">
-                        <Col>
-                            <p className="ml-5 ">{this.state.textEntered.toUpperCase()}</p>
-                        </Col>
-                    </Row> */}
-
-                    <Row className="mt-5">
+                    <Row className="mt-2">
                         <Col md={12} className="mt-5 pt-3 justify-content center">
-                            <Table className="table mx-auto"  style={{ color: "white", }}>
-                                <tr className="text-center">
-                                    <th  >SI NO</th>
-                                    <th>QUALIFICATION</th>
-                                    <th>OPTIONS</th>
-                                </tr>
-                                <tbody className="text-center">
-                                    {this.state.list.map((itm, k) => {
+                            {this.state.list.map((itm, k) => {
                                         return (
-                                            <tr>
-                                                <td>{k+1}</td>
-                                                <td>{itm.toUpperCase()}</td>
-                                                 <td><Button className="pl-4 pr-4 mt-1" color="danger" onClick={()=>{if(window.confirm('Doyou wnat to delete the item?')) {this.delete(k)};}}>DELETE </Button>{' '}
-                                                 <Button className="pl-4 pr-4 mt-1" color="danger" onClick={()=>{if(window.confirm('Do you wnat to edit?')) {this.edit(k)};}} >EDIT </Button></td>
-                                            </tr>
+                                        <ListGroup >
+                                            <ListGroupItem key='k' className="pl-5 mb-4" id="listitem" >{itm.toUpperCase()}
+                                            <i class="far fa-edit" id="edit-icon" onClick={()=>{if(window.confirm('Do you wnat to edit?')) {this.edit(k)};}}></i>
+                                            <i class="fas fa-times" id='delete-icon' onClick={()=>{if(window.confirm('Doyou wnat to delete the item?')) {this.delete(k)};}}></i></ListGroupItem>
+                                          </ListGroup>
+                                            
                                         )
                                     })}
-                                </tbody>
-                            </Table>
                         </Col>
                     </Row>
-
                 </Container>
 
             </React.Fragment>
